d1364df3091a3a9401c34548458334d33bb3ec5a
SlinkierElm5611 <sbalta@evertz.com>
2023-05-22 09:28:03 -0400

    Re-added colour scheme

--- lua/SlinkierElm5611/packer.lua
+++ lua/SlinkierElm5611/packer.lua
@@ -1,6 +1,7 @@
 vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
+    use 'folke/tokyonight.nvim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
