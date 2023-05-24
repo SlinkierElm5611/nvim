ba8697d879172da316bb14f00feeac263f5fc9ed
SlinkierElm5611 <sbalta@evertz.com>
2023-05-23 22:51:03 -0400

    added tab colouring

--- lua/SlinkierElm5611/packer.lua
+++ lua/SlinkierElm5611/packer.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'folke/tokyonight.nvim'
+    use "lukas-reineke/indent-blankline.nvim"
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         requires = { {'nvim-lua/plenary.nvim'} }
