975072100865f15834124b53fc56f3e31e35dae3
SlinkierElm5611 <stefan.balta.sb@gmail.com>
2024-03-14 09:16:11 -0400

    updated noice config

--- after/plugin/config.lua
+++ after/plugin/config.lua
@@ -10,69 +10,69 @@ require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 -- lint
 local lint = require('lint')
 lint.linters_by_ft = {
-    python = {'pylint'},
-		cpp = {'cppcheck'},
+	python = {'pylint'},
+	cpp = {'cppcheck'},
 }
 
 vim.api.nvim_create_autocmd({"BufWritePost"}, {
-    callback =  function ()
-        lint.try_lint()
-    end,
+	callback =  function ()
+		lint.try_lint()
+	end,
 })
 
 -- telescope
 local telescope = require('telescope')
 telescope.setup{
-    pickers = {
-        find_files = {
-            hidden = true,
-        },
-        git_files = {
-            hidden = true,
-        },
-        grep_string = {
-            hidden = true,
-        },
-    },
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+		git_files = {
+			hidden = true,
+		},
+		grep_string = {
+			hidden = true,
+		},
+	},
 }
 
 -- treesitter
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "rust", "python", "c", "cpp", "javascript", "yaml", "bash", "lua" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  },
+	ensure_installed = { "rust", "python", "c", "cpp", "javascript", "yaml", "bash", "lua" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+		max_file_lines = nil,
+	},
 }
 
 -- rainbow delimiters
 local rainbow_delimiters = require 'rainbow-delimiters'
 
 vim.g.rainbow_delimiters = {
-    strategy = {
-        [''] = rainbow_delimiters.strategy['global'],
-        vim = rainbow_delimiters.strategy['local'],
-    },
-    query = {
-        [''] = 'rainbow-delimiters',
-        lua = 'rainbow-blocks',
-    },
-    highlight = {
-        'RainbowDelimiterRed',
-        'RainbowDelimiterYellow',
-        'RainbowDelimiterBlue',
-        'RainbowDelimiterOrange',
-        'RainbowDelimiterGreen',
-        'RainbowDelimiterViolet',
-        'RainbowDelimiterCyan',
-    },
+	strategy = {
+		[''] = rainbow_delimiters.strategy['global'],
+		vim = rainbow_delimiters.strategy['local'],
+	},
+	query = {
+		[''] = 'rainbow-delimiters',
+		lua = 'rainbow-blocks',
+	},
+	highlight = {
+		'RainbowDelimiterRed',
+		'RainbowDelimiterYellow',
+		'RainbowDelimiterBlue',
+		'RainbowDelimiterOrange',
+		'RainbowDelimiterGreen',
+		'RainbowDelimiterViolet',
+		'RainbowDelimiterCyan',
+	},
 }
 
 -- blamer
@@ -82,26 +82,26 @@ vim.cmd[[BlamerShow]]
 vim.cmd[[colorscheme tokyonight-night]]
 vim.opt.termguicolors = true
 local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
 }
 
 local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
 
@@ -111,64 +111,81 @@ require("startup").setup({theme = 'evil'})
 
 -- hypersonic
 require('hypersonic').setup({
-    border = 'rounded',
-    winblend = 0,
-    add_padding = true,
-    hl_group = 'Keyword',
-    wrapping = '"'
+	border = 'rounded',
+	winblend = 0,
+	add_padding = true,
+	hl_group = 'Keyword',
+	wrapping = '"'
 })
 
 -- lualine
 require('lualine').setup {
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
-    },
-    sections = {
-        lualine_a = {'location'},
-        lualine_b = {'branch'},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {'encoding',  'filetype'},
-        lualine_z = {'filename'}
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		}
+	},
+	sections = {
+		lualine_a = {'location'},
+		lualine_b = {'branch'},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {'encoding',  'filetype'},
+		lualine_z = {'filename'}
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = {'filename'},
+		lualine_x = {'location'},
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {}
 }
 
 -- noice
-require('noice').setup()
+require("noice").setup({
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		},
+	},
+	-- you can enable a preset for easier configuration
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+		command_palette = true, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = false, -- add a border to hover docs and signature help
+	},
+})
 
 -- devicons
 require'nvim-web-devicons'.setup {
-    default = true;
+	default = true;
 }
 
 -- ollama gen-nvim
 require("gen").setup({
-    model="codellama"
+	model="codellama"
 })
