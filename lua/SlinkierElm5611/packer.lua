3ad10c418c3753c1ceff86db81fde30fc5b53eda
SlinkierElm5611 <stefan.balta.sb@gmail.com>
2025-05-16 10:31:35 -0400

    Removed lsp-zero in favour of vim built in lsp

--- lua/SlinkierElm5611/packer.lua
+++ lua/SlinkierElm5611/packer.lua
@@ -19,25 +19,12 @@ return require('packer').startup(function(use)
     }
     use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use ('tpope/vim-fugitive')
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end},
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
-    }
+		use {'neovim/nvim-lspconfig'}
+		use {"williamboman/mason.nvim"}
+		use {"williamboman/mason-lspconfig.nvim"}
+		use {"hrsh7th/nvim-cmp"}
+		use {"hrsh7th/cmp-nvim-lsp"}
+		use {"L3MON4D3/LuaSnip"}
     use 'mfussenegger/nvim-lint'
     --   use "mfussenegger/nvim-dap"
     --   use "jay-babu/mason-nvim-dap.nvim"
